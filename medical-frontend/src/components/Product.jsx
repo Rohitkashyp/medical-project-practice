df2d58cab3b819799c177465c6f3013f5bd5e134
Rohitkashyp <ronaldobravo0001@gmail.com>
2025-04-23 00:19:27 -0700

    vercel.json and vite.config added, now error is coming in developement mode

--- medical-frontend/src/components/Product.jsx
+++ medical-frontend/src/components/Product.jsx
@@ -6,12 +6,13 @@ import axios from "axios";
 const Product = () => {
  
   const [products,setProducts] = useState([])
+  const [isloading,setIsloading] = useState(true)
 
   const navigate = useNavigate()
 
   const fetchproducts = async() => {
     const res = await axios.get("https://medical-project-practice.onrender.com/products")
-   
+    setIsloading(false)
     setProducts(res.data)
   }
 
@@ -27,26 +28,36 @@ const Product = () => {
 
 
   return (
-    <section id="products" className="py-10">
-      <h2 className="text-2xl font-bold text-center mb-6">Our Products</h2>
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
-        {products.map((product) => (
-          <div key={product.id} className="border p-4 rounded">
-            <h3 className="text-lg font-bold">{product.name}</h3>
-            <p>{product.description}</p>
-            <p className="font-bold">{product.price}</p>
-            <button
-              className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
-              onClick={() =>{navigate('/order-place',{state:{productName:product.name}})}}
-            >
-              Order Now
-            </button>
-          </div>
-        ))}
-      </div>
-
-     
-    </section>
+    <>
+    {
+      isloading ? (
+       <div className="flex justify-center items-center max-w-[1200px] mx-auto mt-6">
+        <p className="text-2xl">Loading....</p>
+       </div>
+      ) : (
+        <section id="products" className="py-10">
+        <h2 className="text-2xl font-bold text-center mb-6">Our Products</h2>
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+          {products.map((product) => (
+            <div key={product.id} className="border p-4 rounded">
+              <h3 className="text-lg font-bold">{product.name}</h3>
+              <p>{product.description}</p>
+              <p className="font-bold">{product.price}</p>
+              <button
+                className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
+                onClick={() =>{navigate('/order-place',{state:{productName:product.name}})}}
+              >
+                Order Now
+              </button>
+            </div>
+          ))}
+        </div>
+  
+       
+      </section>
+      )
+    }
+    </>
   );
 };
 
